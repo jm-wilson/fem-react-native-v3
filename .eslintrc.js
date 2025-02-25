e2b86e62007084e65dd56b3920fd1d9eee2bc33d
Joe Wilson <joe.wilson@jwils.xyz>
2025-02-25 09:54:25 -0500

    `npm run lint-fix`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
   },
-  ignorePatterns: ['/dist/*'],
+  ignorePatterns: ["/dist/*"],
 };
