d850c7267efd2e83bf4b5f7ef328454781a1d0b1
Joe Wilson <joe.wilson@jwils.xyz>
2025-02-25 11:07:50 -0500

    Update `index.tsx` and add styles with `StyleSheet.create`

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./theme";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  itemContainer: {
+    justifyContent: "center",
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: 200,
+  },
+});
